1d167481ab968d9bd5e730a951d5abe40ed99c0c
Igor Siemienowicz <igor@qprise.com>
2024-03-04 15:26:22 +0800

    1 Basic Stub (#2)
    * basic application all set up.
    * add a logo and some 'about' text

---
+++ application.py
@@ -0,0 +1,25 @@
+import toga
+
+class EnChat(toga.App):
+    """The main application class
+    
+    Attributes:
+        main_window (toga.MainWindow): The main window of the application
+    """
+
+    def startup(self) -> None:
+        """Build the GUI elements of the application, in including the main winddow
+
+        This method is called to start the application.
+        """
+
+        self.main_window = toga.MainWindow()
+        self.main_window.show()
+
+        return super().startup()
+    
+def main():
+    return EnChat("enChat", "com.technopraxia.enchat", description="Interaction with OpenAI LLMs")
+
+if __name__ == "__main__":
+    main().main_loop()
